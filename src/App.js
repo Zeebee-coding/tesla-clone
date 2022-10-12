d566f66769403cfd0e3df3e86d606174cb8cea40
zeebee <cocobee818942@gmail.com>
2022-10-12 18:15:19 +0500

    ADD:Sections for the TESLA

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+// import logo from './logo.svg';
+// import { Counter } from './features/counter/Counter';
+// import Header from './components/Header';
+import Home from './components/Home';
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      {/* <Header/> <br></br> */}
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
